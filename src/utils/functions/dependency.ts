ce7c8554db691bb48e96b1dff71ffeea39dd1e37
fvivaudo <fvivaudo@student.42.fr>
2024-10-17 02:33:01 +0200

    Explicit ts-toolbelt import

--- src/utils/functions/dependency.ts
+++ src/utils/functions/dependency.ts
@@ -1,4 +1,4 @@
-import { F } from 'ts-toolbelt'
+import { Function } from 'ts-toolbelt'
 import { container, InjectionToken } from 'tsyringe'
 
 export async function resolveDependency<T>(token: InjectionToken<T>, interval: number = 500): Promise<T> {
@@ -10,7 +10,7 @@ export async function resolveDependency<T>(token: InjectionToken<T>, interval: n
 
 type Forward<T> = { [Key in keyof T]: T[Key] extends abstract new (...args: any) => any ? InstanceType<T[Key]> : T[Key] }
 
-export async function resolveDependencies<T extends readonly [...unknown[]]>(tokens: F.Narrow<T>) {
+export async function resolveDependencies<T extends readonly [...unknown[]]>(tokens: Function.Narrow<T>) {
 	return Promise.all(tokens.map((token: any) =>
 		resolveDependency(token)
 	)) as Promise<Forward<F.Narrow<T>>>
